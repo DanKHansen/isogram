968a48bea69ba089630d24ca13f09769b37f5511
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-15 12:00:12 +0200

    refactor

--- src/main/scala/Isogram.scala
+++ src/main/scala/Isogram.scala
@@ -1,2 +1,2 @@
 object Isogram:
-   def isIsogram(s: String): Boolean = s.filter(_.isLetter).toLowerCase.length == s.filter(_.isLetter).toLowerCase.distinct.length
+   def isIsogram(s: String): Boolean = s.filter(_.isLetter).toLowerCase == s.filter(_.isLetter).toLowerCase.distinct
